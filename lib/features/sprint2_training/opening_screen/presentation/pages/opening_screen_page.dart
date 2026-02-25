ac8c14b56f3eee147b3b3c7bf89a48b2ecc78480
Ervin, Nathan A <ervin54@iastate.edu>
2026-02-25 17:03:43 -0600

    Finished second video, working through the place holder and now buttons soon. Will remove uneeded code once complete

--- lib/features/sprint2_training/opening_screen/presentation/pages/opening_screen_page.dart
+++ lib/features/sprint2_training/opening_screen/presentation/pages/opening_screen_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:socialdeck/design_system/index.dart';
 
 class OpeningScreenPage extends StatelessWidget{
   const OpeningScreenPage({super.key});
@@ -6,10 +7,38 @@ class OpeningScreenPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(children: [
-          Text("Opening Screen")
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch, 
+          children: [
+        //------------------------ Top Navigation ------------------------//
+            // const SDeckTopNavigationBar.titleOnly(title: 'Hello Testing!'),
+        //------------------------ Visual Placeholder --------------------------//
+        Padding( 
+          padding: const EdgeInsets.all(SDeckSpace.padding16), 
+          child: buildVisualPlaceholder(context)
+        ),
+
+        SizedBox(height: SDeckSpace.gap16)
+
       ],),),
 
       );
+  }
+
+  //*************************** Helper Methods ********************************//
+  //------------------------ Visual Placeholder ----------------------------//
+  Widget buildVisualPlaceholder(BuildContext context) {
+    return Container(
+      width: 370,
+      height: 370,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(SDeckRadius.borderRadius16),
+        image: const DecorationImage(
+          image: AssetImage(SDeckIcon.checkeredBackground),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }
