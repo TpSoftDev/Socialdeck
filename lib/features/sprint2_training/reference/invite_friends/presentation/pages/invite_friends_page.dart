a6c31750a12798b0b2d52a6ee5d0aa8f4b5c1dd2
Ervin, Nathan A <ervin54@iastate.edu>
2026-02-25 13:10:34 -0600

    Made branch for work, checked out example

--- lib/features/sprint2_training/reference/invite_friends/presentation/pages/invite_friends_page.dart
+++ lib/features/sprint2_training/reference/invite_friends/presentation/pages/invite_friends_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:socialdeck/design_system/index.dart';
 
-
+//Checked out example (will remove this comment later)
 
 class InviteFriendsPage extends StatelessWidget {
   const InviteFriendsPage({super.key});
