6eedf6a678c1e18284769b11afac4877d17d5bfd
RaynHylance <jreynoldsalar@gmail.com>
2026-04-18 17:24:38 -0500

    Remove commented out dev buttons as well as unused import

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:socialdeck/config/routes/constants/route_constants.dart';
 import 'package:socialdeck/design_system/index.dart';
 import 'package:socialdeck/shared/providers/auth_state_provider.dart';
 import '../../../onboarding/shared/services/google_auth_service.dart';
@@ -90,34 +89,6 @@ class _HomePageState extends ConsumerState<HomePage> {
                       size: SDeckButtonSize.large,
                       onPressed: () => context.push('/test/profile-card'),
                     ),
-                    SizedBox(height: SDeckSpace.gap16),
-                    // Dev-only: remove before shipping. Onboarding: introduce profile card (+ merged add-photo UI).
-                    /*SDeckSolidButton(
-                      text: 'Dev: Introduce profile card',
-                      size: SDeckButtonSize.large,
-                      onPressed: () =>
-                          ref.watch(currentUserProvider)!.delete()
-                    ),
-                    SizedBox(height: SDeckSpace.gap16),
-                    // Dev-only: remove before shipping. Opens profile onboarding redirect screen.
-                    SDeckSolidButton(
-                      text: 'Dev: Profile redirecting',
-                      size: SDeckButtonSize.large,
-                      onPressed: () => context.push('/profile/redirecting'),
-                    ),
-                    SizedBox(height: SDeckSpace.gap16),
-                    SDeckSolidButton(
-                      text: 'Dev: Edit Photo',
-                      size: SDeckButtonSize.large,
-                      onPressed: () => context.push(AppPaths.editPhoto),
-                    ),
-                    SizedBox(height: SDeckSpace.gap16),
-                    SDeckSolidButton(
-                      text: 'Dev: Unable to Continue',
-                      size: SDeckButtonSize.large,
-                      onPressed: () =>
-                          context.push(AppPaths.unableToContinue),
-                    ),*/
                     SizedBox(height: SDeckSpace.gap16),
                     SDeckSolidButton(
                       text: 'Logout',
